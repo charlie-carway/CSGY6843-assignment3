a74139876daa6ad372bcd65dad7206a91f850627
Charles Carway <78627703+charlie-carway@users.noreply.github.com>
2022-02-27 22:21:14 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -38,30 +38,46 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     # Fill in start                         ***** START ******
     mail_from = "Mail From:<cc2033@nyu.edu>\r\n"
     clientSocket.send(mail_from)
-    rec_from = clientSocket.recv(1024)
-    rec_from = rec_from.decode()
-    print("Outcome MAIL FROM: "+rec_from)
-    # Fill in end                         ***** END ******
+    recv_from = clientSocket.recv(1024)
+    print("MAIL FROM status: "+recv_from.decode())
+    # Fill in end                           ***** END ******
 
     # Send RCPT TO command and handle server response.
-    # Fill in start
-    # Fill in end
+    # Fill in start                         ***** START ******
+    rcpt_to = "RCPT TO:<cc@gmail.com>\r\n"
+    clientSocket.send(rcpt_to)
+    recv_to = clientSocket.recv(1024)
+    print("RCPT TO command status: "+recv_to.decode())
+    # Fill in end                           ***** END ******
 
     # Send DATA command and handle server response.
-    # Fill in start
-    # Fill in end
+    # Fill in start                         ***** START ******
+    data = "DATA\r\n"
+    clientSocket.send(data)
+    recv_data = clientSocket.recv(1024)
+    print("SEND DATA command status: "+recv_data.decode())
+    # Fill in end                           ***** END ******
 
     # Send message data.
-    # Fill in start
-    # Fill in end
+    # Fill in start                         ***** START ******
+    clientSocket.send(msg)
+
+    # Fill in end                           ***** END ******
 
     # Message ends with a single period, send message end and handle server response.
-    # Fill in start
-    # Fill in end
+    # Fill in start                         ***** START ******
+    clientSocket.send(endmsg)
+    # Fill in end                           ***** END ******
 
     # Send QUIT command and handle server response.
-    # Fill in start
-    # Fill in end
+    # Fill in start                         ***** START ******
+    quit = "QUIT\r\n"
+    clientSocket.send(quit)
+    recv_quit = clientSocket.recv(1024)
+    print(recv_quit.decode())
+    clientSocket.close()
+    exit(0)
+    # Fill in end                           ***** END ******
 
 
 if __name__ == '__main__':
